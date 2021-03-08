5efd2e20e954f7b373862d260291b593aadafa3d
Luis Choma <luisp.choma@gmail.com>
2021-03-07 21:00:10 -0300

    Respond getAll users route accordingly

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -4,20 +4,12 @@ export const userController = {
   getAllUsers: (req, res) => {
     getAll(req.con, (error, results) => {
       if (error) console.log('Erro ao fazer query ao banco')
-
-      req.con.end()
-
-      return results
+      res.send(results)
     })
-    res.end()
   },
   createUser: (req, res) => {
     create(req.con, req.body, (error, results) => {
-      if (error) console.log('Erro ao fazer cadastro no banco')
-
-      req.con.end()
-
-      return results
+      if (error) console.log('Erro ao fazer cadastro no banco ', error)
     })
     res.end()
   },
