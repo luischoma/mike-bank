0b83bb63fa8819f63209f09b419e91746bed9a8c
Luis Choma <luisp.choma@gmail.com>
2021-03-07 21:01:52 -0300

    Refactor get all users query to use sql-query

--- server/src/model/user.js
+++ server/src/model/user.js
@@ -1,5 +1,15 @@
+import sql from 'sql-query'
+
+const sqlQuery = sql.Query()
+const sqlSelect = sqlQuery.select()
+const sqlInsert = sqlQuery.insert()
+
 export const getAll = (connection, callback) => {
-  connection.query('SELECT * FROM user', callback)
+  const selectQuery = sqlSelect.from('user').build()
+
+  connection.query(selectQuery, callback)
+
+  connection.end()
 }
 
 export const create = (connection, body, callback) => {
