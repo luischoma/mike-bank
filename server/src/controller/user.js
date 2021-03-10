432132d989c6de3cc1c934c36ca2e34239517acd
Luis Choma <luisp.choma@gmail.com>
2021-03-09 23:46:30 -0300

    Add authenticate method on controller

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,7 +1,10 @@
 import {
   create,
   getById,
+  authenticate,
 } from '../model/user.js'
+
+import md5 from 'md5'
 
 export const userController = {
   createUser: (req, res) => {
@@ -14,6 +17,20 @@ export const userController = {
     getById(req.con, req.params, (error, results) => {
       if (error) console.log('Erro ao fazer query ao banco')
       res.send(results)
+    })
+  },
+
+  authenticateUser: (req, res) => {
+    authenticate(req.con, req.body, (error, results) => {
+      if (error) console.log('Erro ao consultar banco ', error)
+
+      results[0].password === md5(req.body.password)
+        ? res.status(200).send({
+            status: 'Authenticated',
+          })
+        : res.status(401).send({
+          status: 'Unauthenticated',
+        })
     })
   },
 }
