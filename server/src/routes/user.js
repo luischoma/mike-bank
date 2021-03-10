dede164729560285f776799f1da12edf17fd7e86
Luis Choma <luisp.choma@gmail.com>
2021-03-09 23:45:53 -0300

    Create login route

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -6,5 +6,6 @@ const userRouter = express.Router()
 userRouter.get('/:id', userController.getUserById)
 
 userRouter.post('/', userController.createUser)
+userRouter.post('/login', userController.authenticateUser)
 
 export default userRouter
