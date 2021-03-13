5e8253639881126e3c22f7fb19edee98b9bb3da6
Luis Choma <luisp.choma@gmail.com>
2021-03-13 03:25:20 -0300

    Add Input component atom

--- client/src/components/atoms/input.js
+++ client/src/components/atoms/input.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import snakeCase from 'lodash/snakeCase'
+
 import './input.scss'
 
 export const Input = ({ textLabel }) => {
   return (  
     <div className="input">
-        <label for={textLabel} className="input__label">
+        <label for={snakeCase(textLabel)} className="input__label">
             {textLabel}
         </label>
-        <input type="text" id={textLabel} className="input__textfield"/>
+        <input type="text" id={snakeCase(textLabel)} className="input__textfield"/>
     </div>
     )
 }
