4480819110b9ba05b74bfca6ff7520913e3ae247
Luis Choma <luisp.choma@gmail.com>
2021-03-14 12:15:58 -0300

    Modify Title component

--- client/src/components/atoms/title.js
+++ client/src/components/atoms/title.js
@@ -6,7 +6,7 @@ export const Title = ({ name }) => {
   return (  
     <div className="title">
       <p className="title__name">
-        {name}
+        {`${name},`}
       </p>
       <p className="title__description">
             suas opções são:
