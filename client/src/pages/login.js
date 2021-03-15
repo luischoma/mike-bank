a32974e5814253fa6fac5ea4464207750bcbdad4
Luis Choma <luisp.choma@gmail.com>
2021-03-14 21:21:39 -0300

    Create Login page

---
+++ client/src/pages/login.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import {
+  AnonymousTemplate,
+} from '../components/templates'
+
+import {
+  PageTitle,
+  Input,
+  Button
+} from '../components/atoms'
+
+import {
+  Form
+} from '../components/organisms'
+
+
+export const Login = ({ children }) => {
+  return (
+    <div className='login'>
+      <AnonymousTemplate className='login__template'>
+        <PageTitle className='login__title' text='entrar' />
+        <Form>
+          <Input textLabel='login'/>
+          <Input textLabel='senha'/>
+
+          <Button textButton='foi'/>
+          <a href='google.com'> não tem conta? registre-se</a>
+        </Form>
+      </AnonymousTemplate>
+    </div>
+  )
+}
