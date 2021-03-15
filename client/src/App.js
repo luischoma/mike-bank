9312315ad5d1a95bcc94e5543bae2ad2db5fe195
Luis Choma <luisp.choma@gmail.com>
2021-03-15 01:09:41 -0300

    Create Login and Register routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,26 @@ import { Route, Switch, BrowserRouter} from 'react-router-dom'
 import '../src/api/config/config.js'
 import 'typeface-saira'
 import './App.css'
-import { Button, Input } from './components/atoms'
+
+import {
+  Login,
+  Register
+} from './pages/'
 
 function App() {
   return (
-    <div className="App">
-             <Button >
-               Oiiii
-             </Button>
-             <Input textLabel='General' />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
