55e2b23d6f8c23e1a45b4ed704caaafa8c7a1f32
Luis Choma <luisp.choma@gmail.com>
2021-03-15 04:27:22 -0300

    Add behavior on Withdrawal page

--- client/src/pages/withdrawal.js
+++ client/src/pages/withdrawal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 
 import {
@@ -8,21 +8,61 @@ import {
 import {
   PageTitle,
   Input,
-  Button
+  Button,
+  ErrorMessage,
+  SuccessMessage
 } from '../components/atoms'
 
 import {
   Form
 } from '../components/organisms'
 
+import {
+  useTransaction
+} from '../hooks/use-transaction'
+
 // eslint-disable-next-line
 export const Withdrawal = ({ children }) => {
+  const [dataState, setDataState] = useState()
+  const [errorState, setErrorState] = useState()
+
+
+  const {
+    data,
+    error,
+    executePost
+  } = useTransaction()
+
+
+  useEffect(() => {
+    if (data) {
+      setDataState(true)
+      setErrorState(false)
+    }
+
+    if (error) {
+      setDataState(false)
+      setErrorState(true)
+    }
+  },[data, error])
+
   return (
     <div className='withdrawal'>
       <LoggedInTemplate className='withdrawal__template'>
         <PageTitle className='withdrawal__title' text='Retirar' />
-        <Form>
+        <Form onSubmit={([amount]) => executePost({
+          data: {
+            kind: 'WITHDRAWAL',
+            amount: -Math.abs(parseInt(amount[1]))
+          },
+          headers: {
+            Authorization: `Bearer ${window.sessionStorage.getItem('mikeBankToken')}`
+          }
+        })}>
           <Input textLabel='quantidade'/>
+          {dataState && <SuccessMessage text='você sacou seu dinheiro com sucesso' />}
+          {/* TODO: Redirect automatically :( */}
+          {errorState && <ErrorMessage text='não foi possível processar seu pagamento. talvez você precise logar novamente.' />}
           <Button textButton='retirar'/>
         </Form>
       </LoggedInTemplate>
