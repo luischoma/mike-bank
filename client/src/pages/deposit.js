faee041bc5f78b2adabcf0ef01e585d2ebd4b8e9
Luis Choma <luisp.choma@gmail.com>
2021-03-15 05:11:11 -0300

    Finish deposit page

--- client/src/pages/deposit.js
+++ client/src/pages/deposit.js
@@ -8,21 +8,41 @@ import {
 import {
   PageTitle,
   Input,
-  Button
+  Button,
+  ErrorMessage
 } from '../components/atoms'
 
 import {
   Form
 } from '../components/organisms'
 
+import {
+  useTransaction
+} from '../hooks/use-transaction'
+
 // eslint-disable-next-line
 export const Deposit = ({ children }) => {
+  const {
+    error,
+    executePost
+  } = useTransaction()
+
   return (
     <div className='deposit'>
       <LoggedInTemplate className='deposit__template'>
         <PageTitle className='deposit__title' text='Depósito' />
-        <Form>
+        <Form onSubmit={([amount]) => executePost({
+          data: {
+            kind: 'DEPOSIT',
+            amount: Math.abs(parseInt(amount[1]))
+          },
+          headers: {
+            Authorization: `Bearer ${window.sessionStorage.getItem('mikeBankToken')}`
+          }
+        })}>
           <Input textLabel='quantidade'/>
+
+          {error && <ErrorMessage text='não foi possível processar seu pagamento. talvez você precise logar novamente.' />}
           <Button textButton='depositar'/>
         </Form>
       </LoggedInTemplate>
