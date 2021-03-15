3b0659b6443d5d09a67d98fdc6179de3c95f4243
Luis Choma <luisp.choma@gmail.com>
2021-03-14 22:20:13 -0300

    Create handler function on Form

--- client/src/components/organisms/form.js
+++ client/src/components/organisms/form.js
@@ -3,9 +3,24 @@ import PropTypes from 'prop-types'
 
 import './form.scss'
 
-export const Form = ({ children }) => {
+
+
+export const Form = ({ children, onSubmit }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const normalizedForm =  Array.from(event).filter(
+      (target) => target.tagName === "INPUT"
+    ).map(
+      (target) => [target.id, target.value]
+    )
+
+    onSubmit(normalizedForm)
+  }
+
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={handleSubmit}>
       {children}
     </form>
   )
