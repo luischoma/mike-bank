ae6725806c29ac55acb0bc65917b90a83fed5711
Luis Choma <luisp.choma@gmail.com>
2021-03-15 01:30:43 -0300

    Use useRegister hook on Register page

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -8,25 +8,44 @@ import {
 import {
   PageTitle,
   Input,
-  Button
+  Button,
+  ErrorMessage
 } from '../components/atoms'
 
 import {
   Form
 } from '../components/organisms'
 
+import {
+  useRegister
+} from '../hooks/use-register'
+
 // eslint-disable-next-line
 export const Register = ({ children }) => {
+  const {
+    error,
+    executePost
+  } = useRegister()
+
   return (
     <div className='register'>
       <AnonymousTemplate className='register__template'>
         <PageTitle className='register__title' text='cadastro' />
-        <Form>
+        <Form onSubmit={([login, password, balance]) => executePost({
+          data: {
+            name: login[1],
+            password: password[1],
+            balance: balance[1]
+          }
+        })}>
           <Input textLabel=' seu login'/>
           <Input textLabel='sua senha'/>
           <Input textLabel='quanto de dinheiro vc quer ter de início'/>
 
+          {error && <ErrorMessage text='não sei dizer qual, mas algum campo é inválido' />}
+
           <Button textButton='cadastrar!'/>
+
         </Form>
       </AnonymousTemplate>
     </div>
