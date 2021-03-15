b054ac5090872bc0a308b5510637de20d5e492b6
Luis Choma <luisp.choma@gmail.com>
2021-03-14 21:02:46 -0300

    Add styles to AnonymousTemplate

--- client/src/components/templates/anonymous-template.js
+++ client/src/components/templates/anonymous-template.js
@@ -10,6 +10,9 @@ export const AnonymousTemplate = ({ className = '', children }) => {
   return (
     <div className={`anonymous-template ${className}`}>
       <LogoHeader />
+      <div className='anonymous-template__body'>
+        {children}
+      </div>
     </div>
   )
 }
